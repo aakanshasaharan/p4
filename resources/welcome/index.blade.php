30d6dfc6a3e3dfaa0a2cac7b4623ec3638951e52
Aakansha <aakansha.saharan@gmail.com>
2015-12-18 13:25:48 -0500

    Update index.blade.php

--- resources/welcome/index.blade.php
+++ resources/welcome/index.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('content')
-<h2>IndianRecipe Master</h2>
+<h1>IndianRecipe Master</h1>
     <p>
         Welcome to IndianRecipe Master, a personal recipes organizer.
         To get started <a href='/login'>log in</a> or <a href='/register'>register</a>.
